58678cbfb9930474eae77b409076c86c36a729fb
Michael Shields <mshields@srcnrg.com>
2017-06-18 22:57:17 -0600

    Cleanup UI

--- src/main/java/com/iwellsite/homework/controllers/ChatController.java
+++ src/main/java/com/iwellsite/homework/controllers/ChatController.java
@@ -26,9 +26,7 @@ public class ChatController {
 
     @MessageMapping("/chat/{topic}")
     @SendTo("/topic/messages")
-    public OutputMessage send(
-            @DestinationVariable("topic") String topic, Message message)
-            throws Exception {
+    public OutputMessage send(@DestinationVariable("topic") String topic, Message message) throws Exception {
         return new OutputMessage(message.getFrom(), message.getText(), topic);
     }
 }
--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,9 +1,12 @@
 /**
  * Created by mshields on 2017-06-17.
  */
-var stompClient = null;
+var stompClient = null,
+    idOfCurrrentlySelectedChatRoom = -666,
+    nameOfCurrrentlySelectedChatRoom = ""; // Globals === BAD, I know...
 
 function setConnected(connected) {
+    // TODO: Review whether these are needed/wanted anymore
     $("#connect").prop("disabled", connected);
     $("#disconnect").prop("disabled", !connected);
     if (connected) {
@@ -22,13 +25,13 @@ function connect() {
         setConnected(true);
         console.log('Connected: ' + frame);
 
+        // TODO: Consider removing or re-using this as a signaling channel
         stompClient.subscribe('/topic/greetings', function (greeting) {
             showGreeting(JSON.parse(greeting.body).content);
         });
 
         stompClient.subscribe('/topic/messages', function (message) {
             showMessage(JSON.parse(message.body));
-            //showGreeting(JSON.parse(message.body).content);
         });
 
         $("#conversation").show();
@@ -50,7 +53,7 @@ function disconnect() {
 function sendName() {
     stompClient.send("/app/hello", {}, JSON.stringify({'name': $("#name").text()}));
 
-    var topic = $('#topic').val(),
+    var topic = nameOfCurrrentlySelectedChatRoom,
         from = $("#name").text(),
         text = $('#msg').val();
     stompClient.send("/app/chat/" + topic, {}, JSON.stringify({ from: from, text: text }));
@@ -61,7 +64,7 @@ function showGreeting(message) {
 }
 function showMessage(msg)
 {
-    $('#messages').append('<tr>' +
+    $('#chatRoomHistoryTableBody_' + idOfCurrrentlySelectedChatRoom).append('<tr>' +
         '<td>' + msg.from + '</td>' +
         '<td>' + msg.topic + '</td>' +
         '<td>' + msg.message + '</td>' +
@@ -69,12 +72,42 @@ function showMessage(msg)
         '</tr>');
 }
 
+$(".btn.btn-primary").on("click", function() {
+    roomSelected($(this).children()); // i.e. button
+});
+
+function roomSelected(btn) {
+    var id = btn.data("chatRoomId");
+    idOfCurrrentlySelectedChatRoom = id;
+    nameOfCurrrentlySelectedChatRoom = btn.data("chatRoomName");
+    window.chatRooms.forEach(function(chatRoom) {
+        if (id === chatRoom.id) {
+            showRoom(chatRoom.id);
+        } else {
+            hideRoom(chatRoom.id);
+        }
+    });
+}
+function showRoom(roomId) {
+    $("#chatRoomHistory_" + roomId).removeClass('hidden');
+}
+function hideRoom(roomId) {
+    $("#chatRoomHistory_" + roomId).addClass('hidden');
+}
+
 $(function () {
     $("form").on('submit', function (e) {
         e.preventDefault();
     });
-    $( "#send" ).click(function() { sendName(); });
+    $( "#send" ).click(function() {
+        sendName();
+        $("#msg").val("");
+    });
     connect();
     $(window).bind('beforeunload', disconnect);
+    if (window.chatRooms && window.chatRooms.length > 0) {
+        idOfCurrrentlySelectedChatRoom = window.chatRooms[0].id;
+        nameOfCurrrentlySelectedChatRoom = window.chatRooms[0].name;
+    }
 });
 
